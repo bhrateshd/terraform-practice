0cd7e5ea8aa28efc47254625507f0d47cb23bba9
Bhratesh <bhrateshd@gmail.com>
2024-08-16 12:19:51 +0530

    moving and adding files

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,27 +1,12 @@
-variable "ami_value" {
-    description = "Value for the AMI"
-}
-
-variable "instance_type_value" {
-    description = " Value for instance type"
-  
-}
-
-variable "subnet_id_value" {
-    description = "value for subnet id"
-  
-}
-
-
 provider "aws" {
     region = "ap-south-1"
     
 }
 
-resource "aws_instance" "jenkinsmaster" {
-    ami = var.ami_value
-    instance_type = var.instance_type_value
-    subnet_id = var.subnet_id_value
-
+module "ec2_instance" {
+    source = "./modules/ec2_instance"
+    ami_value = ""
+    instance_type_value = ""
+    subnet_id_value = ""
   
 }
