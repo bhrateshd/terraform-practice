41f272e6bd433b3d097204aad385817c5eaf75d7
Bhratesh <bhrateshd@gmail.com>
2024-08-17 14:28:17 +0530

    Day 5

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1"
   map_public_ip_on_launch = true
 }
 
