bbb676df3639251ab2a60441f9186a34f5455dcd
Bhratesh <bhrateshd@gmail.com>
2024-09-12 17:53:57 +0530

    adding the main file

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -1,14 +1,9 @@
-provider "aws" {     # this is a provide on which we are gonna create a machine
-    region = "ap-south-1"
-    
-  # Configuration options
+provider "aws" {
+  region = "ap-south-1"
+  
 }
 
 resource "aws_instance" "myserver" {
-    ami = "ami-0ad21ae1d0696ad58"
-    instance_type = "t2.micro"
-
-    tags = {
-      Name = "SampleServer"
-    }
+  ami = "#add ami id "
+  instance_type = "t2.micro" 
 }
